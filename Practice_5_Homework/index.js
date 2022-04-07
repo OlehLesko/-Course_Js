61e23e96af645b7624e42be75b95c2210523cdc2
OlehLesko <olehlesko81@gmail.com>
2022-04-07 19:00:55 +0300

    Make 5 task and edit 2

--- Practice_5_Homework/index.js
+++ Practice_5_Homework/index.js
@@ -14,11 +14,16 @@
 
 
 // Task 2
-// const arr = [];
+// const arr_keys = [];
+// const arr_values = [];
 
 // function showProps(obj) {
-//     arr.push(obj)
-//     console.log(arr);
+//     const obj_keys = Object.keys(obj);
+//     const obj_values = Object.values(obj);
+//     arr_keys.push(obj_keys);
+//     arr_values.push(obj_values);
+//     console.log(arr_keys);
+//     console.log(arr_values);
 // };
 
 // const obj = {
@@ -29,7 +34,7 @@
 //     learning: true
 // };
 
-// showProps(obj)
+// showProps(obj);
 
 // Task 3 
 // class Person {
@@ -66,103 +71,105 @@
 // stud1.showCourse(6);
 
 // Task 4 
-const arrSalaries = [];
-const arrNames = [];
+// const arrSalaries = [];
+// const arrNames = [];
 
-class Worker {
-    #privateField = 1.2;
+// class Worker {
+//     #privateField = 1.2;
 
-    constructor(fullName, dayRate, workingDays) {
-        this.fullName = fullName;
-        this.dayRate = dayRate;
-        this.workingDays = workingDays;
-    };
+//     constructor(fullName, dayRate, workingDays) {
+//         this.fullName = fullName;
+//         this.dayRate = dayRate;
+//         this.workingDays = workingDays;
+//     };
 
-    set setExp(value){
-        this.#privateField = value;
-    };
+//     set setExp(value){
+//         this.#privateField = value;
+//     };
 
-    get showExp() {
-        return this.#privateField;
-    }
+//     get showExp() {
+//         return this.#privateField;
+//     }
 
-    showSalary = () => {
-        const salary = this.dayRate * this.workingDays;
-        console.log(`${this.fullName} salary : ${salary}` );
-    };
+//     showSalary = () => {
+//         const salary = this.dayRate * this.workingDays;
+//         console.log(`${this.fullName} salary : ${salary}` );
+//     };
 
-    showSalaryWithExperience() {
-        const salary = this.dayRate * this.workingDays * this.#privateField;
-        console.log(`${this.fullName} salary : ${salary}`);
+//     showSalaryWithExperience() {
+//         const salary = this.dayRate * this.workingDays * this.#privateField;
+//         console.log(`${this.fullName} salary : ${salary}`);
 
-        if(this.#privateField == 1.5) {
-            arrSalaries.push(salary);
+//         if(this.#privateField == 1.5) {
+//             arrSalaries.push(salary);
            
-            arrSalaries.sort(function(a, b){ 
-                return a - b
-            });
+//             arrSalaries.sort(function(a, b){ 
+//                 return a - b
+//             });
             
-            arrNames.push(this.fullName)
+//             arrNames.push(this.fullName)
 
-            const fromIndex = arrNames.indexOf('Tom Tomson'); 
-            const toIndex = 2;
+//             const fromIndex = arrNames.indexOf('Tom Tomson'); 
+//             const toIndex = 2;
 
-            const element = arrNames.splice(fromIndex, 1)[0];
+//             const element = arrNames.splice(fromIndex, 1)[0];
 
-            arrNames.splice(toIndex, 2, element);   
+//             arrNames.splice(toIndex, 2, element);   
             
-        };
+//         };
 
-    };   
+//     };   
 
-    sort() {
-        console.log("Sorted salary:");
-        for(let i = 0; i != arrSalaries.length; i++) {
-            console.log(`${arrNames[i]}: ${arrSalaries[i]}`)
-        };
-    }
+//     sort() {
+//         console.log("Sorted salary:");
+//         for(let i = 0; i != arrSalaries.length; i++) {
+//             console.log(`${arrNames[i]}: ${arrSalaries[i]}`)
+//         };
+//     }
 
    
-};
+// };
 
-let worker1 = new Worker("John Johnson", 20, 23);
-console.log(worker1.fullName);
-worker1.showSalary();
-console.log("New expirience: " + worker1.showExp)
-worker1.showSalaryWithExperience();
-worker1.setExp = 1.5;
-console.log("New experience: " + worker1.showExp);
-worker1.showSalaryWithExperience();
+// let worker1 = new Worker("John Johnson", 20, 23);
+// console.log(worker1.fullName);
+// worker1.showSalary();
+// console.log("New expirience: " + worker1.showExp)
+// worker1.showSalaryWithExperience();
+// worker1.setExp = 1.5;
+// console.log("New experience: " + worker1.showExp);
+// worker1.showSalaryWithExperience();
 
-console.log('')
+// console.log('')
 
-let worker2 = new Worker("Tom Tomson", 48, 22);
-console.log(worker2.fullName);
-worker2.showSalary();
-console.log("New expirience: " + worker2.showExp)
-worker2.showSalaryWithExperience();
-worker2.setExp = 1.5;
-console.log("New experience: " + worker2.showExp);
-worker2.showSalaryWithExperience();
-console.log('')
+// let worker2 = new Worker("Tom Tomson", 48, 22);
+// console.log(worker2.fullName);
+// worker2.showSalary();
+// console.log("New expirience: " + worker2.showExp)
+// worker2.showSalaryWithExperience();
+// worker2.setExp = 1.5;
+// console.log("New experience: " + worker2.showExp);
+// worker2.showSalaryWithExperience();
+// console.log('')
 
-let worker3 = new Worker("Andy Ander", 29, 23);
-console.log(worker3.fullName);
-worker3.showSalary();
-console.log("New expirience: " + worker3.showExp)
-worker3.showSalaryWithExperience();
-worker3.setExp = 1.5;
-console.log("New experience: " + worker3.showExp);
-worker3.showSalaryWithExperience();
+// let worker3 = new Worker("Andy Ander", 29, 23);
+// console.log(worker3.fullName);
+// worker3.showSalary();
+// console.log("New expirience: " + worker3.showExp)
+// worker3.showSalaryWithExperience();
+// worker3.setExp = 1.5;
+// console.log("New experience: " + worker3.showExp);
+// worker3.showSalaryWithExperience();
 
 
-console.log('')
+// console.log('')
 
-worker3.sort() // Sort
+// worker3.sort() // Sort
 
 
 
 // Task 5
+// let total_area = 0;
+
 // class GeometricFigure {
 //     getArea() {
 //         return 0;
@@ -185,7 +192,8 @@ worker3.sort() // Sort
 //         super.getArea();
 
 //         const area = (this.base * this.height) / 2;
-//         console.log(area);
+//         console.log(`Geometric figure: Triangle - area: ${area}`)
+//         total_area += area;
 //     }
 // };
 
@@ -200,7 +208,8 @@ worker3.sort() // Sort
 //         super.getArea()
     
 //         const areaOfSquare = this.squareLength * this.squareLength;
-//         console.log("Area of square is: " + areaOfSquare);
+//         console.log(`Geometric figure: Square - area: ${areaOfSquare}`);
+//         total_area += areaOfSquare;
 //     };
 // };
 
@@ -216,40 +225,45 @@ worker3.sort() // Sort
 
 //         let pi = 3.14159265358979323846;
 //         const findArea = pi * this.radius * this.radius;
-//         console.log(findArea)
+//         console.log(`Geometric figure: Circle - area: ${findArea}`)
+//         total_area += findArea;
 
 //     }
 // };
-// // const f = new Circle(5);
-// // f.getArea();
 
-// function handleFigures(figures) {
-//     // const openValues = figures;
-//     // console.log(openValues)
+
+// function handleFigures(figures) {    
 
 //     for(let i = 0; i != figures.length; i++) {
-//         if (figures[i].prototype == 'GeometricFigure'){
-//             console.log(true);
-//         }
-//         // console.log(GeometricFigure.hasOwnProperty(figures[i]));
-//         console.log(Triangle.prototype)
-        
+//         switch (true) {
+//             case figures[i] == 'Triangle':
+//                 let index_element_triangle = figures.indexOf(figures[i]);
+            
+//                 const create_figures_triangle = new Triangle(figures[index_element_triangle].base,
+//                      figures[index_element_triangle].height)
+//                 create_figures_triangle.getArea();
+                
+//                 break;
+//             case figures[i] == 'Square':
+//                 let index_element_square = figures.indexOf(figures[i]);
+            
+//                 const create_figures_square = new Square(figures[index_element_square].squareLength)
+//                 create_figures_square.getArea();
+                
+//                 break;
+//             case figures[i] == 'Circle':
+//                 let index_element_circle = figures.indexOf(figures[i]);
+                        
+//                 const create_figures_circle = new Circle(figures[index_element_circle].radius)
+//                 create_figures_circle.getArea();
+                
+//                 break;
+//         }  
 //     };
-//     // console.log(figures)
-//     // console.log(figures.hasOwnProperty('GeometricFigure'));
-//     // console.log(Element.prototype.figures)
+//     console.log(`Total area : ${total_area}`);
 
 // };
-// // 
-// //hasOwnProperty
-// //reduse
 
 
 // const figures = [new Triangle(4, 5), new Square(7), new Circle(5)];
-// // console.log(handleFigures(figures));
 // handleFigures(figures)
-
-
-// // // let obj = {name: 'ol'}
-// // // obj.age = 24
-// // // console.log(obj)
