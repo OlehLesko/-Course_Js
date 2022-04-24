d55292ff2bbd4547d1476f19d4e59d5d95d67cc1
OlehLesko <olehlesko81@gmail.com>
2022-04-25 01:28:05 +0300

    Doned task 2

--- Practice_8_Homework/index.js
+++ Practice_8_Homework/index.js
@@ -1,13 +1,26 @@
 // Task 1
-function upperCase(word){
-    const worCheck = /^[A-Z]/
-    const result = worCheck.test(word)
-    if(result === true) {
-        console.log("String's starts with uppercase character");
-    } else {
-        console.log("String's not starts with uppercase character");
-    };
-};
+// function upperCase(word){
+//     const worCheck = /^[A-Z]/
+//     const result = worCheck.test(word)
+//     if(result === true) {
+//         console.log("String's starts with uppercase character");
+//     } else {
+//         console.log("String's not starts with uppercase character");
+//     };
+// };
 
-upperCase('regexp');
-upperCase('RegExp');
+// upperCase('regexp');
+// upperCase('RegExp');
+
+
+// Task 2
+// const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+
+// function checkEmail(text) {
+//     console.log(regex.test(text))
+// };
+
+// checkEmail("Qmail2@gmail.com");
+// // Test emails
+// checkEmail("workingexample@email.com");
+// checkEmail("notworking@2.com");
