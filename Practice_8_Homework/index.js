95bfebbf9d6c6333a8d900ae6b259bb7d605d409
OlehLesko <olehlesko81@gmail.com>
2022-04-26 13:51:35 +0300

    Made practice 8

--- Practice_8_Homework/index.js
+++ Practice_8_Homework/index.js
@@ -24,3 +24,68 @@
 // // Test emails
 // checkEmail("workingexample@email.com");
 // checkEmail("notworking@2.com");
+
+
+//Task 3
+// function regExp(text){
+//     const result_arr = [];
+
+//     const reg_exp = /d(b+)(d)/ig;
+//     const result = reg_exp.exec(text);
+
+//     result_arr.push(result[0], result[1], result[2])
+//     console.log(result_arr);
+// }
+
+// regExp("cdbBdbsbz");
+
+
+// Task 4
+// const reg_exp = /(\w+)\s(\w+)/;
+// const str = "Java Script";
+// const result = str.replace(reg_exp, "$2, $1");
+// console.log(result);
+
+
+// Task 5
+// function valid_card_number(cardNumber) {
+//     const reg_exp = /\d{4}-\d{4}-\d{4}-\d{4}/;
+//     const result = reg_exp.test(cardNumber);
+    
+//     result ? console.log("Card number correct") 
+//             : console.log("Card number not correct");
+// };
+
+// valid_card_number("9999-9999-9999-9999");
+
+
+// Task 6
+// function checkEmail(emailAdress) {
+//   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+//   if (emailAdress.match(regexEmail)) {
+//       console.log("Email is correct!");
+//   } else {
+//       console.log("Email is not correct!");
+//   }
+// }
+
+// checkEmail('my_mail@gmail.com');
+
+// checkEmail('#my_mail@gmail.com');
+
+// checkEmail('my_ma--il@gmail.com');
+
+
+// Task 7
+// function validateUserName(username){
+//     const usernameRegex = /^[^\d][^!@#\$%\^&\*\(\)_\+-=]\w+\.?\w+/;;
+//     const regex = /[+-]?\d+(\.\d+)?/g;
+//     const result = usernameRegex.test(username);
+//     const matches = username.match(regex);  
+
+//     console.log(result);
+//     console.log(matches);
+// }
+// validateUserName('ee1.1ret3')
+// validateUserName('ee1*1ret3');
