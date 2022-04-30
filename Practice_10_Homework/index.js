a05315425b680fc5a79d089d015e99bb60d065bc
OlehLesko <olehlesko81@gmail.com>
2022-05-01 02:05:33 +0300

    Maked 1 n 2  tasks

---
+++ Practice_10_Homework/index.js
@@ -0,0 +1,23 @@
+// Task 1
+// const arr = ["Tom", "Sam", "Ray", "Bob"];
+// const [x, y, , ...z] = arr;
+
+// console.log(x); 
+// console.log(y); 
+// console.log(z); 
+
+
+// Task 2 
+// let data = {
+//     names: ["Sam", "Tom", "Ray", "Bob"],
+//     ages: [20, 24, 22, 26],
+// };
+
+// const [ , name2, , name4] = data.names;
+// const [ , age2, , age4] = data.ages;
+
+// console.log(name2); 
+// console.log(age2); 
+// console.log(name4); 
+// console.log(age4); 
+ 
