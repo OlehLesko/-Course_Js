a25ea69d06adcaa7591afd084158ce71afa07ca0
OlehLesko <olehlesko81@gmail.com>
2022-05-01 15:33:57 +0300

    Made all tasks

---
+++ Practice_9_Homework/index.js
@@ -0,0 +1,24 @@
+// Task 1
+// $("a[href^='https://']").attr('target', '_blank');
+
+
+// Task 2 
+// const $txt_head = $('h2.head');
+// $txt_head.css('background', 'green');
+// $txt_head.find('inner').css('fontSize', '35px')
+
+
+// Task 3 
+// $('h3 + div').each(function() {
+//     $(this).prev().before($(this));
+// });
+
+
+// Task 4 
+// function onclickCheckbox(){
+//     if ($(':checkbox:checked').length == 3){
+//         $(':checkbox').attr('disabled', 'true');
+//     }
+// };
+
+// $(':checkbox').on('click', onclickCheckbox);
